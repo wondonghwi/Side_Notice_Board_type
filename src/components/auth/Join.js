88f920abf9f4cb3fa4ef6c563c765ac397b1608b
donghwi <ehdehd1289@naver.com>
2020-11-11 23:39:38 +0900

    yarn add axios , Join.js + axios post

--- src/components/auth/Join.js
+++ src/components/auth/Join.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import axios from 'axios';
+
+const LOCAL_URL = 'http://localhost:8000';
 
 const Join = () => {
+
+  const onJoin = async () => {
+
+    const sendObject = {
+      email : '',
+      username : '',
+      password : '',
+    };
+
+    const sendString = JSON.stringify(sendObject);
+
+    //데이터 생성 요청
+    axios.post(`${LOCAL_URL}/v1/auth/sign-in/` , sendString);
+  }
+
   return (
     <div>
         <h1>회원가입</h1>
@@ -16,7 +34,7 @@ const Join = () => {
         <div>
           <input type="password" placeholder='password를 입력해주세요'/>
         </div>
-        <button>Join</button>
+        <button onClick={onJoin}>Join</button>
     </div>
   );
 };
